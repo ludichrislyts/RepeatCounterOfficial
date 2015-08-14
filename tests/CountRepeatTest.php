6779bfffcd0252ce9d484a992455db5b33f85056
Chris Lytsell <ludichrislyts@gmail.com>
2015-08-14 11:21:19 -0700

    pass fourth spec test

--- tests/CountRepeatTest.php
+++ tests/CountRepeatTest.php
@@ -48,5 +48,20 @@
 			//Assert
 			$this->assertEquals($answer, $result);
 		}
+		//spec 4 test
+		function test_singleLetterMultTimesInString()
+		{
+			//Arrange
+			$test = new CountRepeat;
+			$input_word = "I";
+			$input_string = "I hope I get a job after class";
+
+			//Act
+			$result = $test->getWordCount($input_word, $input_string);
+			$answer = 2;
+
+			//Assert
+			$this->assertEquals($answer, $result);
+		}
 	}
 ?>
